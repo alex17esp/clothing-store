69186094341d810f3390a6f8db6d066aa9c2c1cd
Alex Doran <alex17esp@gmail.com>
2020-06-17 19:22:57 -0400

    implemented firebase utils, included ability to store authenticated user to firestore db

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,23 @@ const config = {
   measurementId: "G-JT65DB78QF",
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({ displayName, email, createdAt, ...additionalData });
+    } catch (error) {
+      console.log("Error creating User", error.message);
+    }
+  }
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
--- src/pages/sign-in-register/sign-in-register.component.jsx
+++ src/pages/sign-in-register/sign-in-register.component.jsx
@@ -3,9 +3,12 @@ import React from "react";
 import SignIn from "../../components/sign-in/sign-in.component";
 import "./sign-in-register.style.scss";
 
+import SignUp from "../../components/sign-up/sign-up.component";
+
 const SignInAndRegisterPage = () => (
   <div className="sign-in-and-register">
     <SignIn />
+    <SignUp />
   </div>
 );
 
